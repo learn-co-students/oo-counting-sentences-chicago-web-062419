04eb2eb459e8c14ef4b01db628c2d0c11e899b43
github username <github email address>
2019-06-29 13:02:17 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,21 @@ require 'pry'
 
 class String
 
+
+  # Do not need to pass through a string as an argument because the string which calls on the method will inherently have the .end_with? method at the back of it
   def sentence?
-    
+    end_with?(".")
   end
 
   def question?
-
+    end_with?("?")
   end
 
   def exclamation?
-
+    end_with?("!")
   end
 
   def count_sentences
-
+     split(/[?!.] /).count
   end
 end
